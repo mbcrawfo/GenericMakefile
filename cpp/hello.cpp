16bb41245791d387156890e20e687fa52e244624
Saikat Bishal <2017ugmm037@nitjsr.ac.in>
2020-10-04 09:40:27 +0530

    new commit

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -4,7 +4,13 @@ int main(int argc, char* argv[])
 {
   // suppress warnings
   (void)argc; (void)argv;
-
+  int args;
   std::cout << "Hello World!" << std::endl;
+  std::cout << "Enter an integer";
+  std::cin >> args;
+  while(args--)
+  {
+    std::cout << "C++ while loop being executed";
+  }
   return 0;
 }
